62130f5837dcaeb270b4ae195e7347a20b2d5889
Alexandra Smolyanskaya <alex.smoly@gmail.com>
2014-10-31 19:28:22 -0400

    added static google map display of top 5 hotspots

--- getNewBirds.py
+++ getNewBirds.py
@@ -3,7 +3,11 @@ from pprint import pprint
 import requests
 import math
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+from pylab import * # "core parts of numpy, scipy, and matplotlib" (http://wiki.scipy.org/PyLab)
+from cStringIO import StringIO
+from PIL import Image
+import urllib
 
 
 def ebird(service, **reqParams):
@@ -19,31 +23,93 @@ def ebird(service, **reqParams):
         return resp.json()
 
 
+def google_geo(location):
+    # Google location lookup by name
+    info = requests.get(
+            'https://maps.googleapis.com/maps/api/geocode/json?address='+location+'&sensor=false').json()
+
+    geo = {'formatted_address': info['results'][0]['formatted_address'],
+        'lat': info['results'][0]['geometry']['location']['lat'],
+        'lng': info['results'][0]['geometry']['location']['lng']}
+
+    return geo
+
+def google_map(good_hs, bad_hs):
+    # show a Google map!
+
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'black']
+
+    # if isinstance(location, str):
+    #     geo = google_geo(location)
+    #     lat = geo['lat']
+    #     lng = geo['lng']
+    # else:
+    #     lat = location[0]
+    #     lng = location[1]
+
+    # sort hotspots by number of birds! TODO: do this in find_good_hotspots, also sort p array
+    good_hs = sorted(good_hs, key=lambda n: n['expected_n'])
+
+    if len(good_hs) > 6:
+        use_hs = good_hs[0:6]
+    else:
+        use_hs = good_hs
+
+    print(len(use_hs))
+
+
+    # Generate URL with markers
+    base_url = 'http://maps.googleapis.com/maps/api/staticmap?&size=1000x1000&sensor=false]'
+
+    url_param = ''
+    # markers for good hotspots
+    url_param = '&markers=size:mid%%7Ccolor:%s' %colors[0]
+    ind = 0
+    for x in use_hs:
+        url_param += '&markers=size:mid%%7Ccolor:%s%%7Clabel:%i' %(colors[ind], ind+1)
+        url_param += '%%7C%f,%f' %(x['lat'],x['lng'])
+        ind += 1
+
+    url_param += '&markers=size:tiny%%7Ccolor:%s' % colors[-1]
+    for x in bad_hs[1:20]: #FIXME: make reasonable estimate of size
+        url_param+='%%7C%f,%f' %(x['lat'],x['lng'])
+
+
+    image_bytes = urllib.urlopen(base_url+url_param).read()
+    image = Image.open(StringIO(image_bytes)) #StringIO makes file object out of image data
+
+
+    # TODO
+    # - take in lat,lng of hotspots
+    # - plot hotspot markers, sorted by # expected new birds.
+
+
+    Image._show(image)
+
+    return image
+
+
 def get_new_birds(here, there, distance):
     # get list of birds that have been observed 'there' but not 'here'
     # 'here' and 'there' should be google-able location names
     # distance in km
     # usage: new_birds = get_new_birds(here, there, distance)
 
-    here_geo = requests.get(
-        'https://maps.googleapis.com/maps/api/geocode/json?address='+here+'&sensor=false').json()
-    # pprint(here_geo['results']) for a somewhat clearer print
+    here_geo = google_geo(here)
+    there_geo = google_geo(there)
 
-    there_geo = requests.get(
-        'https://maps.googleapis.com/maps/api/geocode/json?address='+there+'&sensor=false').json()
+    print 'Finding birds within %.2f' % distance + ' km of ' + there_geo['formatted_address']
+    print 'that are not found within %.2f' % distance + ' km of ' + here_geo['formatted_address']
 
-    print 'Finding birds within %.2f' % distance + ' km of ' + there_geo['results'][0]['formatted_address']
-    print 'that are not found within %.2f' % distance + ' km of ' + here_geo['results'][0]['formatted_address']
-
-    there_geo['results'][0]['geometry']['location']['lat']
+    # there_geo['results'][0]['geometry']['location']['lat']
 
     here_sightings = ebird('data/obs/geo/recent',
-                          lat=here_geo['results'][0]['geometry']['location']['lat'],
-                          lng=here_geo['results'][0]['geometry']['location']['lng'],
+                          lat=here_geo['lat'],
+                          lng=here_geo['lng'],
                           dist=distance)
     there_sightings = ebird('data/obs/geo/recent',
-                           lat=there_geo['results'][0]['geometry']['location']['lat'],
-                           lng=there_geo['results'][0]['geometry']['location']['lng'],
+                           lat=there_geo['lat'],
+                           lng=there_geo['lng'],
                            dist=distance)
 
     # Allow for missing common name, returns 'None', missing names bug fixed by eBird on 9/8
@@ -58,10 +124,10 @@ def get_new_birds(here, there, distance):
     return new_birds
 
 def get_hotspots(location, distance):
-    # returns hotspot names and IDs within 'distance' of 'location'
+    # returns all hotspot names and IDs within 'distance' of 'location'
     # location can be googlable name
     # distance in km
-    # usage: hotspots = et_hotspots(location, distance)
+    # usage: hotspots = get_hotspots(location, distance)
 
     print 'Finding hotspots in %s' %location
 
@@ -79,18 +145,17 @@ def get_hotspots(location, distance):
         back=15, # look back only 15 days
         fmt='csv')
 
-    # find state hotspots within distance km
-    # TODO add distance
-    hotspots = [[x['locName'], x['locID']] for x in state_hs if get_distance(origin, [x['lat'], x['lng']]) < distance]
+    # find state hotspots within distance (km)
+    hotspots = [[x['locName'], x['locID'], x['lat'], x['lng']] for x in state_hs if get_distance(origin, [x['lat'], x['lng']]) < distance]
 
     return hotspots
 
 
 def get_distance(origin, destination):
     # compute great-circle distance using haversine formula
-    # 'origin' and 'destination' should be [lat, long] lists
+    # 'origin' and 'destination' should be [lat, lng] lists
     # Author: Wayne Dyck
-    # usage: d = get_distance(origin [lat,lng], destination [lat, long])
+    # usage: d = get_distance(origin [lat,lng], destination [lat, lng])
 
     lat1, lon1 = origin
     lat2, lon2 = destination
@@ -160,6 +225,10 @@ def plot_hotspots(good_hotspots, prob_array, titles):
     # plot heatmap of bird probabilities
     # usage: plot_hotspots(good_hotspots, prob_array, titles)
 
+    if len(good_hotspots) < 1:
+        print 'No hotspots provided to plot_hotspots'
+        return
+
     # set up axes and labels
     fig, axes = plt.subplots(1, 1, figsize=(20, 8),
          subplot_kw={
@@ -187,7 +256,7 @@ def plot_hotspots(good_hotspots, prob_array, titles):
     [i.set_color("red") for i in plt.gca().get_xticklabels() if i.get_text() in all_notable]
 
     plt.title(titles['title'])
-    plt.show()
+    show()
 
 
 def find_good_hotspots(here, there, distance):
@@ -195,14 +264,16 @@ def find_good_hotspots(here, there, distance):
     # also return sightings data array for plotting
     # 'here' and 'there' should be google-able location names, e.g. "Philadelphia"
     # distance in km
-    # usage: [good_hotspots, prob_array, bird_list] = find_good_hotspots(here, there, distance)
+    # usage: [good_hotspots, bad_hotspots, prob_array, bird_list] = find_good_hotspots(here, there, distance)
 
     bird_list = get_new_birds(here, there, distance)
-    hotspots = get_hotspots(there, distance)
+    hotspot_ids = get_hotspots(there, distance)
 
     good_hotspots = []
+    bad_hotspots = []
+
     prob_array = []
-    for x in hotspots:
+    for x in hotspot_ids:
         num_seen = get_counts(bird_list, x[1])
 
         ind_max = num_seen.argmax()
@@ -223,24 +294,42 @@ def find_good_hotspots(here, there, distance):
             good_hotspots.append({
                 'locName': x[0],
                 'locID': x[1],
+                'lat': x[2],
+                'lng': x[3],
                 'birds': keep_birds,
                 'notable': notable_birds,
                 'expected_n': np.sum(prob_seen)})
 
-            # print good_hotspots[-1]['locName']
-            # print '  Most common bird: ' + most_common[0] + ', reported %s' %most_common[1] + ' times in the last 30 days'
-            # for i in good_hotspots[-1]['birds']:
-            #     print '     ' + i[0] + ' prob = %.2f' % i[1]
+            print good_hotspots[-1]['locName']
+            print '  Most common bird: ' + most_common[0] + ', reported %s' %most_common[1] + ' times in the last 30 days'
+            for i in good_hotspots[-1]['birds']:
+                print '     ' + i[0] + ' prob = %.2f' %i[1]
+        else:
+            bad_hotspots.append({
+                'locName': x[0],
+                'locID': x[1],
+                'lat': x[2],
+                'lng': x[3]})
+            print '    X does not have enough sightings of most common bird (%d)' %most_common[1]
 
-    # make array and plot!
+    # make probability array and plot!
     prob_array = np.array(prob_array)
+
+    #TODO 1
+    # sort hotspots ** and prob array?? **
+    # good_hotspots = sorted(good_hotspots, key=lambda n: n['expected_n'])
+
+
+
+    # good_hotspots = sorted(good_hotspots, key=good_hotspots.expected_n)
 
     titles = {'birds': bird_list,
         'hotspots': [x['locName'] for x in good_hotspots],
         'title': 'Probability of observing birds not seen in %s' %here + ' in %s hotspots' %there
     }
+
     plot_hotspots(good_hotspots, prob_array, titles)
+    # google_map(there, good_hotspots, bad_hotspots)
 
-
-    print 'Done!'
-    return good_hotspots, prob_array, bird_list
+    print 'Done! Found %d good hotspots' %len(good_hotspots)
+    return good_hotspots, bad_hotspots, prob_array, bird_list
